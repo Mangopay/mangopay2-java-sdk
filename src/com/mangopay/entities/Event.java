440e23b761461e1c9bd5e454b3d27bc063f453b9
Łukasz Drzewiecki <lukasz.drzewiecki@mosaic-lab.com>
2013-12-11 11:46:19 +0100

    Renamed Event entity property from RessourceId to ResourceId

--- src/com/mangopay/entities/Event.java
+++ src/com/mangopay/entities/Event.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mangopay.entities;
 
 import com.mangopay.core.EntityBase;
@@ -11,7 +7,7 @@ import com.mangopay.core.EntityBase;
  */
 public class Event extends EntityBase {
     
-    public String RessourceId;
+    public String ResourceId;
     
     public String EventType;
     
