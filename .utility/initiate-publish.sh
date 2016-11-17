45ed08b5efd527ae6bbc7fbb53b0388797c94fe8
Paul Hristea <paul.hristea@codegile.com>
2016-11-17 11:11:34 +0200

    Configuration for Sonatype publishing through Travis CI.

---
+++ .utility/initiate-publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script initiates the Gradle publishing task when pushes to master occur.
+# NOTE: Travis-CI can only publish SNAPSHOT versions. To release a version, you need
+#       to use the internal ReadyTalk Jenkins job.
+
+if [ "$TRAVIS_REPO_SLUG" == "Mangopay/mangopay2-java-sdk" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "sonatype" ]; then
+
+  echo -e "Starting publish to Sonatype...\n"
+
+  ./gradlew uploadArchives -PnexusUsername="${SONATYPE_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}" -Psigning.keyId=DA419019 -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=keyfile.gpg
+  RETVAL=$?
+
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed publish!'
+    ./gradlew closeAndPromoteRepository
+  else
+    echo 'Publish failed.'
+    return 1
+  fi
+
+fi
