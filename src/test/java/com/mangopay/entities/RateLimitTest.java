7fa9f11a8c06dffe640b94c7b86622ca690f24b8
Paul Hristea <paul.hristea@codegile.com>
2017-08-01 21:30:26 +0300

    Added a new line.

--- src/test/java/com/mangopay/entities/RateLimitTest.java
+++ src/test/java/com/mangopay/entities/RateLimitTest.java
@@ -21,6 +21,7 @@ public class RateLimitTest extends BaseTest {
         getJohnsAccount();
 
         List<RateLimit> rateLimits = this.api.getRateLimits();
+
         assertNotNull(rateLimits);
         assertTrue(rateLimits.size() == 4);
     }
