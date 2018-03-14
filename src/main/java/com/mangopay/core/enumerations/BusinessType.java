3e22584b046045c8c8a640dd7a939576a8245274
cosmin <comybv95@yahoo.com>
2018-03-14 12:55:11 +0200

    Added .idea to .gitignore Added PlatformCategorization object Removed PlatformType enum

---
+++ src/main/java/com/mangopay/core/enumerations/BusinessType.java
@@ -0,0 +1,27 @@
+package com.mangopay.core.enumerations;
+
+/**
+ * Business type enumeration
+ */
+public enum BusinessType {
+
+    /**
+     * Marketplace business type
+     */
+    MARKETPLACE,
+    /**
+     * Crowdfunding business type
+     */
+    CROWDFUNDING,
+
+    /**
+     * Franchise business type
+     */
+    FRANCHISE,
+
+    /**
+     * Other business type
+     */
+    OTHER
+
+}
