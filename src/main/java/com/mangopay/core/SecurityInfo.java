cdd084c4c36f3264b5e3f8c30c0eead0eb179600
Paula <trefasp@yhaoo.com>
2019-05-15 12:22:49 +0300

    CardPreAuthorization-Api-GSON: fixed AVSResult field

--- src/main/java/com/mangopay/core/SecurityInfo.java
+++ src/main/java/com/mangopay/core/SecurityInfo.java
@@ -1,9 +1,11 @@
 package com.mangopay.core;
 
+import com.google.gson.annotations.SerializedName;
 import com.mangopay.core.enumerations.AVSResult;
 
 public class SecurityInfo extends Dto {
 
+    @SerializedName("AVSResult")
     private AVSResult avsResult;
 
     public AVSResult getAvsResult() {
