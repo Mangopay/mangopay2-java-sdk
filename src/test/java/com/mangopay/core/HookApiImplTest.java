7ac848b80c6672754aed8a5e236b07e8031edee0
Mircea Solovastru <mircea.solo95@yahoo.com>
2020-12-09 21:13:29 +0200

    fixed hooks tests

--- src/test/java/com/mangopay/core/HookApiImplTest.java
+++ src/test/java/com/mangopay/core/HookApiImplTest.java
@@ -3,6 +3,7 @@ package com.mangopay.core;
 import com.mangopay.core.enumerations.EventType;
 import com.mangopay.entities.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
@@ -73,14 +74,31 @@ public class HookApiImplTest extends BaseTest {
     @Test
     public void createUserInflowHooks() {
         try {
-            Hook hook = new Hook();
-            hook.setEventType(EventType.USER_INFLOWS_BLOCKED);
-            hook.setUrl("http://test.com");
-            Hook inflowBlockedHook = this.api.getHookApi().create(hook);
+            List<Hook> allHooks = this.api.getHookApi().getAll(new Pagination(1, 100), null);
+            Hook inflowBlockedHook = null;
+            Hook inflowUnblockedHook = null;
+            for (Hook hook : allHooks) {
+                if (hook.getEventType() == EventType.USER_INFLOWS_BLOCKED) {
+                    inflowBlockedHook = hook;
+                }
+                if (hook.getEventType() == EventType.USER_INFLOWS_UNBLOCKED) {
+                    inflowUnblockedHook = hook;
+                }
+            }
 
-            hook.setEventType(EventType.USER_INFLOWS_UNBLOCKED);
-            hook.setUrl("http://test.com");
-            Hook inflowUnblockedHook = this.api.getHookApi().create(hook);
+            if (inflowBlockedHook == null) {
+                Hook hook = new Hook();
+                hook.setEventType(EventType.USER_INFLOWS_BLOCKED);
+                hook.setUrl("http://test.com");
+                inflowBlockedHook = this.api.getHookApi().create(hook);
+            }
+
+            if (inflowUnblockedHook == null) {
+                Hook hook = new Hook();
+                hook.setEventType(EventType.USER_INFLOWS_UNBLOCKED);
+                hook.setUrl("http://test.com");
+                inflowUnblockedHook = this.api.getHookApi().create(hook);
+            }
 
             assertNotNull(inflowBlockedHook);
             assertNotNull(inflowUnblockedHook);
@@ -92,18 +110,34 @@ public class HookApiImplTest extends BaseTest {
     @Test
     public void createUserOutflowHooks() {
         try {
-            Hook hook = new Hook();
+            List<Hook> allHooks = this.api.getHookApi().getAll(new Pagination(1, 100), null);
+            Hook outflowBlockedHook = null;
+            Hook outlowUnblockedHook = null;
+            for (Hook hook : allHooks) {
+                if (hook.getEventType() == EventType.USER_OUTFLOWS_BLOCKED) {
+                    outflowBlockedHook = hook;
+                }
+                if (hook.getEventType() == EventType.USER_OUTFLOWS_UNBLOCKED) {
+                    outlowUnblockedHook = hook;
+                }
+            }
 
-            hook.setEventType(EventType.USER_OUTFLOWS_BLOCKED);
-            hook.setUrl("http://test.com");
-            Hook outflowBlockedHook = this.api.getHookApi().create(hook);
+            if (outflowBlockedHook == null) {
+                Hook hook = new Hook();
+                hook.setEventType(EventType.USER_OUTFLOWS_BLOCKED);
+                hook.setUrl("http://test.com");
+                outflowBlockedHook = this.api.getHookApi().create(hook);
+            }
 
-            hook.setEventType(EventType.USER_OUTFLOWS_UNBLOCKED);
-            hook.setUrl("http://test.com");
-            Hook outflowUnblockedHook = this.api.getHookApi().create(hook);
+            if (outlowUnblockedHook == null) {
+                Hook hook = new Hook();
+                hook.setEventType(EventType.USER_OUTFLOWS_UNBLOCKED);
+                hook.setUrl("http://test.com");
+                outlowUnblockedHook = this.api.getHookApi().create(hook);
+            }
 
             assertNotNull(outflowBlockedHook);
-            assertNotNull(outflowUnblockedHook);
+            assertNotNull(outlowUnblockedHook);
         } catch (Exception ex) {
             Assert.fail(ex.getMessage());
         }
