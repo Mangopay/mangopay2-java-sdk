1d108736902f1c876de5520c3ae61ce1112e3518
catalin.craciun <craciunctln@gmail.com>
2021-10-04 12:40:44 +0300

    Debugging missing key

--- .utility/initiate-publish.sh
+++ .utility/initiate-publish.sh
@@ -5,6 +5,8 @@
 
 echo -e "Starting publish to Sonatype...\n"
 
+ls -l /home/runner/work/mangopay2-java-sdk/mangopay2-java-sdk/.gnupg
+
 ./gradlew publish -PnexusUsername="${MAVEN_USERNAME}" -PnexusPassword="${SONATYPE_PASSWORD}" -Psigning.keyId=AF02E028 -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=.gnupg/private.key
 RETVAL=$?
 
