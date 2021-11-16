45e929cf202ca6b8546f3d8fe04823711957463e
SoloJr <mircea.solo95@yahoo.com>
2021-11-16 21:09:50 +0200

    Revert last commit

--- src/main/java/com/mangopay/core/enumerations/PayoutMode.java
+++ src/main/java/com/mangopay/core/enumerations/PayoutMode.java
@@ -2,7 +2,5 @@ package com.mangopay.core.enumerations;
 
 public enum PayoutMode {
     STANDARD,
-    INSTANT_PAYMENT,
-    INSTANT_PAYOUT_SUCCEEDED,
-    INSTANT_PAYOUT_FALLBACKED
+    INSTANT_PAYMENT
 }
