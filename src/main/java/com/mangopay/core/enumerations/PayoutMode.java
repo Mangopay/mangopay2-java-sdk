fe832b1cb6c5dc8a3eea22e7f7622418d82042b7
SoloJr <mircea.solo95@yahoo.com>
2021-11-16 21:03:54 +0200

    MPSDK-975: added new payout hooks

--- src/main/java/com/mangopay/core/enumerations/PayoutMode.java
+++ src/main/java/com/mangopay/core/enumerations/PayoutMode.java
@@ -2,5 +2,7 @@ package com.mangopay.core.enumerations;
 
 public enum PayoutMode {
     STANDARD,
-    INSTANT_PAYMENT
+    INSTANT_PAYMENT,
+    INSTANT_PAYOUT_SUCCEEDED,
+    INSTANT_PAYOUT_FALLBACKED
 }
