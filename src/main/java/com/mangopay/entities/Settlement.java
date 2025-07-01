12906c7c9f1ad000335580a850afda5af0fbf459
Iulian Masar <iulian.masar@codegile.com>
2025-07-01 14:39:06 +0300

    added docs

--- src/main/java/com/mangopay/entities/Settlement.java
+++ src/main/java/com/mangopay/entities/Settlement.java
@@ -4,27 +4,54 @@ import com.google.gson.annotations.SerializedName;
 import com.mangopay.core.EntityBase;
 
 public class Settlement extends EntityBase {
+    /**
+     * The unique identifier of the settlement object
+     */
     @SerializedName("SettlementId")
     private String settlementId;
 
+    /**
+     * The status of the settlement
+     */
     @SerializedName("Status")
     private String status;
 
+    /**
+     * The date at which the settlement was created by the external provider
+     */
     @SerializedName("SettlementDate")
     private String settlementDate;
 
+    /**
+     * The name of the external provider
+     */
     @SerializedName("ExternalProviderName")
     private String externalProviderName;
 
+    /**
+     * The total amount declared through intent API calls with the following calculation:
+     * (Sum of captured intents) - (Sum of refunds amounts) + (Sum of refund reversed amounts) - (Sum of DISPUTED disputes) + (Sum of WON disputes)
+     */
     @SerializedName("DeclaredIntentAmount")
     private Integer declaredIntentAmount;
 
+    /**
+     * The total fees charged by the external provider
+     */
     @SerializedName("ExternalProcessorFeesAmount")
     private Integer externalProcessorFeesAmount;
 
+    /**
+     * The total amount due to the platform (to be held in escrow wallet).
+     * This amount correspond to the TotalSettlementAmount from the settlement file.
+     * A negative amount will result in this parameter being set to zero, indicating no incoming funds to the escrow wallet.
+     */
     @SerializedName("ActualSettlementAmount")
     private Integer actualSettlementAmount;
 
+    /**
+     * The difference between ActualSettlementAmount and the amount received on the escrow wallet
+     */
     @SerializedName("FundsMissingAmount")
     private Integer fundsMissingAmount;
 
