9d7463e368510746ffb8c9f6aed1474c3bfd564f
Iulian <40763147+iulian03@users.noreply.github.com>
2025-09-23 12:08:39 +0300

    [release] 2.55.3 (#434)
    * moved ProfilingAttemptReference to base PayIn class (#431)
    
    * added intent and split event types (#430)
    
    * added VirtualAccountPurpose to BankingAlias & re-enabled tests (#433)

--- src/main/java/com/mangopay/entities/BankingAlias.java
+++ src/main/java/com/mangopay/entities/BankingAlias.java
@@ -1,5 +1,6 @@
 package com.mangopay.entities;
 
+import com.google.gson.annotations.SerializedName;
 import com.mangopay.core.EntityBase;
 import com.mangopay.core.enumerations.BankingAliasType;
 import com.mangopay.core.enumerations.CountryIso;
@@ -43,6 +44,9 @@ public class BankingAlias extends EntityBase {
      * Whether the banking alias is active or not.
      */
     public boolean Active;
+
+    @SerializedName("VirtualAccountPurpose")
+    private String virtualAccountPurpose;
 
     /**
      * Type-specific details of the banking alias.
@@ -103,6 +107,15 @@ public class BankingAlias extends EntityBase {
 
     public void setDetails(BankingAliasDetails details) {
         this.Details = details;
+    }
+
+    public String getVirtualAccountPurpose() {
+        return virtualAccountPurpose;
+    }
+
+    public BankingAlias setVirtualAccountPurpose(String virtualAccountPurpose) {
+        this.virtualAccountPurpose = virtualAccountPurpose;
+        return this;
     }
 
     /**
